11611b5e337d638c59fbad3ce7dbc2e4fa448f39
mrdsx <andre0.castlegames@gmail.com>
2026-06-17 22:01:48 +0300

    refac(backend): improve password matching

--- backend/src/core/middleware.py
+++ backend/src/core/middleware.py
@@ -1,3 +1,5 @@
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 from fastapi import Request, status
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
@@ -7,14 +9,22 @@ from src.core.settings import settings
 # TODO
 # ? Rate limiting (for preventing brute-force)
 # ? Logging       (for auditing auth attempts)
-# ? Long token - must-have
-# ? Timing attack prevention
+
+hasher = PasswordHasher()
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
         auth_token = request.headers.get("auth-token")
-        if auth_token != settings.auth_token:
+        if auth_token is None:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content={"detail": "Invalid credentials."},
+            )
+
+        try:
+            hasher.verify(hash=settings.auth_token, password=auth_token)
+        except VerifyMismatchError:
             return JSONResponse(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 content={"detail": "Invalid credentials."},
