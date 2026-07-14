251e81e1cd335ccf47278920efd1faf4bfd2c363
mrdsx <andre0.castlegames@gmail.com>
2026-07-14 16:15:50 +0300

    chore(backend): update request attempt 'host' field

--- backend/src/db/request_attempts.py
+++ backend/src/db/request_attempts.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.db import Base
@@ -10,6 +10,6 @@ class DB_RequestAttempt(Base):
     __tablename__ = "request_attempts"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    host: Mapped[str]
+    host: Mapped[str] = mapped_column(String, unique=True)
     attempts: Mapped[int]
     last_attempt: Mapped[datetime] = mapped_column(DateTime(timezone=True))
