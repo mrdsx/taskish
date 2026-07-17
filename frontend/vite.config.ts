7f77cd374561b22e4519ff25b818ad806bd3030e
mrdsx <andre0.castlegames@gmail.com>
2026-07-17 12:20:35 +0300

    chore(frontend): update base url for assets in 'vite.config.ts'

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: process.env.NODE_ENV === "production" ? "/static/frontend/" : "/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
