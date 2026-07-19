4d207c59a186b60a5b7f3f52407bf2924b2b7943
mrdsx <andre0.castlegames@gmail.com>
2026-07-19 14:26:11 +0300

    chore: add preview bash script

---
+++ preview.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf backend/static/frontend
+
+# frontend
+cd frontend
+bun run build
+cd ../
+
+# backend
+mv frontend/dist backend/static/frontend
+cd backend
+uv run fastapi dev --host 0.0.0.0
+cd ../
