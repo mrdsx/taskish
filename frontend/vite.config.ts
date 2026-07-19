8c5fef7ed71b4580e5ca2843546131b2d291b28c
mrdsx <andre0.castlegames@gmail.com>
2026-07-19 14:07:23 +0300

    feat(frontend): add PWA generation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,36 @@
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
+  plugins: [
+    solid(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Taskish",
+        short_name: "Taskish",
+        description: "Web app for managing tasks",
+        theme_color: "#000000",
+        background_color: "#000000",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
     proxy: {
